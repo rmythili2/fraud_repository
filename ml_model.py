7fffc2ce7f67e550a3570bc7c424e838459339af
rmythili2 <rmythili2@gmail.com>
2023-11-24 16:14:06 +0530

    added 3 ML model functions

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,15 @@
 
 print("add all model related here")
+
+def model1():
+    print("adding code for ML model1")
+
+    
+def model2():
+    print("adding code for ML model2")
+
+    
+def model3():
+    print("adding code for ML model3")
+
+    
